a2ddbd6333015d109370c9b477ad41a78fc97747
Claudio Gomez <claudio.gomez@gorillalogic.com>
2019-11-07 14:45:13 -0600

    Updated Package file to support Swift 5 tool chain and SPM

--- Package.swift
+++ Package.swift
@@ -1,6 +1,26 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "Freddy"
+    name: "Freddy",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .watchOS(.v2),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(name: "Freddy", targets: ["Freddy"]),
+        .library(name: "Freddy", targets: ["MobileFreddy"]),
+        .library(name: "Freddy", targets: ["TVFreddy"]),
+        .library(name: "Freddy", targets: ["NanoFreddy"])
+    ],
+    targets: [
+        .target(name: "Freddy"),
+        .target(name: "MobileFreddy"),
+        .target(name: "TVFreddy"),
+        .target(name: "NanoFreddy"),
+        .testTarget(name: "FreddyTests", dependencies: ["Freddy"])
+    ]
 )
 
